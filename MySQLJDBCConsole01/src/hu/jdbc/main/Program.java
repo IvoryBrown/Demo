490f96bc88497492469720bdeadbf2be32516dfd
IvoryBrown <paradisho@gmail.com>
2017-08-25 13:23:27 +0200

    JDBC Console sample

--- MySQLJDBCConsole01/src/hu/jdbc/main/Program.java
+++ MySQLJDBCConsole01/src/hu/jdbc/main/Program.java
@@ -32,16 +32,41 @@ public class Program {
 
 			while (rs.next()) {
 
-				String id = rs.getString("orszag");
-				String age = rs.getString("fovaros");
-				String first = rs.getString("foldr_hely");
+				String country = rs.getString("orszag");
+				String capital = rs.getString("fovaros");
+				String geographicLocation = rs.getString("foldr_hely");
+				Integer area = rs.getInt("terulet");
+				String formOfGovernment = rs.getString("allamforma");
+				Integer population = rs.getInt("nepesseg");
+				Integer populationCapital = rs.getInt("nep_fovaros");
+				String carSignal = rs.getString("autojel");
+				String countrys = rs.getString("country");
+				String capitals = rs.getString("capital");
+				String currency = rs.getString("penznem");
+				String moneySign = rs.getString("penzjel");
+				String moneyChanger = rs.getString("valtopenz");
+				Integer phone = rs.getInt("telefon");
+				Integer gdp = rs.getInt("gdp");
+				Integer kat = rs.getInt("kat");
 
-				System.out.print("Ország: " + id);
-				System.out.print(", " + age);
-				System.out.print(", " + first + "\n");
+				System.out.print("Ország: " + country);
+				System.out.print(", Főváros: " + capital);
+				System.out.print(", Földrajzi hely: " + geographicLocation);
+				System.out.print(", Területcapital: " + area);
+				System.out.print(", Államforma: " + formOfGovernment);
+				System.out.print(", Népesség: " + population);
+				System.out.print(", Népesség főváros: " + populationCapital);
+				System.out.print(", Autójel: " + carSignal);
+				System.out.print(", Country: " + countrys);
+				System.out.print(", Capital: " + capitals);
+				System.out.print(", Pénznem: " + currency);
+				System.out.print(", Pénzjel: " + moneySign);
+				System.out.print(", Váltó pénz: " + moneyChanger);
+				System.out.print(", Telefon: " + phone);
+				System.out.print(", Gdp: " + gdp);
+				System.out.println(", Kat: " + kat);
 
 			}
-
 			rs.close();
 			stmt.close();
 			conn.close();
@@ -49,9 +74,7 @@ public class Program {
 			System.err.println("Hiba az adatbázishoz kapcsolódás során!");
 		} catch (Exception e) {
 			System.err.println("Hiba a lekérdezés során");
-			e.printStackTrace();
 		} finally {
-
 			try {
 				if (stmt != null)
 					stmt.close();
@@ -62,9 +85,8 @@ public class Program {
 					conn.close();
 			} catch (SQLException se) {
 				System.err.println("Hiba a lekérdezés során");
-				se.printStackTrace();
 			}
 		}
-		System.out.println("Goodbye!");
+		System.out.println("Viszlát!");
 	}
 }
